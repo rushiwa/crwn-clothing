5f61de1dc4880a543aefcd4fd669a71846295d9e
Rush Limbu <rush.limbu@gamesys.co.uk>
2021-05-17 22:54:32 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import Homepage from './pages/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={ Homepage }/>
+        <Route path='/shop/hats' component={ HatsPage }/>
+      </Switch>
     </div>
   );
 }
