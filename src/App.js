b49d28861d2fb098ca77ca52b4222c99230ad34f
Rush Limbu <rush.limbu@gamesys.co.uk>
2021-06-03 14:16:17 +0100

    saving to transfer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.componet';
+import SignInAndSignUpPage from './pages/signInAndSignUpPage/signInAndSignUpPage.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={ Homepage }/>
         <Route path='/shop' component={ ShopPage }/>
+        <Route path='/signin' component={ SignInAndSignUpPage } />
       </Switch>
     </div>
   );
